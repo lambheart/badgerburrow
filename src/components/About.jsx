2b3731c0ab1216daa2a4c34ff7b6c677b048da28
Celeste Li <celeste_li@icloud.com>
2023-11-04 14:41:28 -0500

    added pages

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React, { useContext, useState, useEffect} from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+
+const About = () => {
+    return(
+        <div>
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useContext, useState, useEffect} from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return(
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar;
