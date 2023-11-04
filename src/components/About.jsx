7cb3a912b69a48d5f051e741282308da1829555c
Celeste Li <celeste_li@icloud.com>
2023-11-04 14:59:28 -0500

    added text contents and navigation bar options

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,22 @@ import "../stylesheets/About.css";
 const About = () => {
     return(
         <div>
-            <h1>About</h1>
+            <h1>Welcome to Study Haven!</h1>
+
+            <h3>Why Study Haven?</h3>
+
+            <p>
+                We are a group of underclassmen that created study haven to help fellow UW-Madison students
+                find cute study spots. As our school grows, it becomes harder and harder to find a spot of your own.
+                We're here to help!
+            </p>
+
+            <h3>How It Works</h3>
+
+            <p>
+                Study Haven will ask you for your current location. Upon receiving your location, it will then recommend a few nearby study spots for you!
+                Ideally, Study Haven will one day be able to find study spots based on your preferences in location or vibe!
+            </p>
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import "../stylesheets/Home.css";
 const Home = () => {
     return(
         <div>
-            <h1>Home</h1>
+            <h1>Study Haven</h1>
         </div>
     )
 }
