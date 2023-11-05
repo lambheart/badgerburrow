0e804f3b4d66d49c037640cf66e85502b8372738
Celeste Li <celeste_li@icloud.com>
2023-11-04 21:21:29 -0500

    add map

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,26 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { Component, useContext, useState, useEffect } from 'react';
 import { Navigate, redirect, useNavigate, Link } from 'react-router-dom';
 import "../App.css";
 import "../stylesheets/Home.css";
 import large_logo from "../assets/large_logo.png";
-import Map from "./Map";
+import MapComponent from "./MapComponent";
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 
-const Home = ({coords}) => {
-    return(
+
+export class Home extends Component {
+    render(coord) {
+        return(
         <div class = "home page">
             <h1></h1>
             <img id = 'large-logo' src = {large_logo} alt = "badger burrow"/>
 
-            <Map coords = {coords} />
+            <Map google = {this.props.google} />
         </div>
-    )
+        );
+        }
 }
 
-export default Home;
+export default GoogleApiWrapper({
+    apiKey: ('AIzaSyB0DSQyxeTXhJzRNEVwQ3khFG7QHX53Yxo')
+})(Home);
