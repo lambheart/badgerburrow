6ad06025e903430824303505a231745680836a48
Celeste Li <celeste_li@icloud.com>
2023-11-04 19:37:18 -0500

    started map component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,16 @@ import { Navigate, redirect, useNavigate, Link } from 'react-router-dom';
 import "../App.css";
 import "../stylesheets/Home.css";
 import large_logo from "../assets/large_logo.png";
+import Map from "./Map";
 
 
-const Home = () => {
+const Home = ({coords}) => {
     return(
         <div>
             <h1></h1>
             <img id = 'large-logo' src = {large_logo} alt = "badger burrow"/>
+
+            <Map coords = {coords} />
         </div>
     )
 }
