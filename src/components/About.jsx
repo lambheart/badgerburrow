6e653e068e4403c6673a7aa62904e3c928b5bb09
Celeste Li <celeste_li@icloud.com>
2023-11-04 23:47:25 -0500

    about page css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,14 +2,15 @@ import React, { useContext, useState, useEffect} from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import "../App.css";
 import "../stylesheets/About.css";
+import large_logo from "../assets/large_logo.png";
 
 
 const About = () => {
     return(
-        <div>
-            <h1>Welcome to Study Haven!</h1>
+        <div class = "about page">
+            <img id = 'large-logo' src = {large_logo} alt = "badger burrow"/>
 
-            <h3>Why Study Haven?</h3>
+            <h3>why badger burrow?!</h3>
 
             <p>
                 We are a group of underclassmen that created study haven to help fellow UW-Madison students
@@ -17,7 +18,7 @@ const About = () => {
                 We're here to help!
             </p>
 
-            <h3>How It Works</h3>
+            <h3>how it works</h3>
 
             <p>
                 Study Haven will ask you for your current location. Upon receiving your location, it will then recommend a few nearby study spots for you!
