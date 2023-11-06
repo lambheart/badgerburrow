d1825f02e5d23c67d8b7fbc336a89a48cd8f19cd
Celeste Li <celeste_li@icloud.com>
2023-11-05 19:49:44 -0600

    finalized css, about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,16 +13,22 @@ const About = () => {
             <h2>why badger burrow?!</h2>
 
             <p>
-                we are a group of underclassmen that created badger burrow to help fellow UW-Madison students
-                find cute study spots. as our school grows, it becomes harder and harder to find a spot of your own.
-                we're here to help!
+                We are a group of underclassmen that created badger burrow to help fellow UW-Madison students
+                find cute study spots. As our school grows, it becomes harder and harder to find a spot of your own.
+                We're here to help!
             </p>
 
             <h2>how it works</h2>
 
             <p>
-                badger burrow will ask you for your current location. upon receiving your location, it will then recommend a few nearby study spots for you!
-                ideally, badger burrow will one day be able to find study spots based on your preferences in location or vibe!
+                badger burrow will recommend a few nearby study spots for you on UW-campus! you can click map markers to look at specific locations within a building
+                Ideally, badger burrow will one day be able to ask you for your current location, find study spots based on your preferences in location or vibe, and take study spot submissions!
+            </p>
+
+            <h2>credits</h2>
+
+            <p>
+                We would like to give credit to dancingbannana on Reddit and their dedication to making education accessible to all Badgers. Without their inital <a href = "https://buckygrades.com/study/">website</a> and <a href= "https://www.reddit.com/r/UWMadison/comments/11wrmr3/i_walked_4648_miles_through_every_building_on/">Reddit post</a>, our project would not have been possible.
             </p>
         </div>
     )
